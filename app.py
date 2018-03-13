daacad5d9749935a5b357e4b42f76d50d986d0ad
izikp <izikp@radware.com>
2018-03-13 12:33:17 +0200

    BUG FIX - Adding $

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ API_KEY = 'AIzaSyDA1cRVAlTLIqZOtev71U8KmURiUG3SIDQ'
 HTML_TEMPLATE = Template("""
 <h1>Hello ${place_name}!</h1>
 
-<img src="https://maps.googleapis.com/maps/api/staticmap?size=700x300&markers=${place_name}&key={api_key}" alt="map of ${place_name}">
+<img src="https://maps.googleapis.com/maps/api/staticmap?size=700x300&markers=${place_name}&key=${api_key}" alt="map of ${place_name}">
 
-<img src="https://maps.googleapis.com/maps/api/streetview?size=700x300&location=${place_name}&key={api_key}" alt="street view of ${place_name}">
+<img src="https://maps.googleapis.com/maps/api/streetview?size=700x300&location=${place_name}&key=${api_key}" alt="street view of ${place_name}">
 """)
 
 
